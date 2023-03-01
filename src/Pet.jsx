31791a6f3d990cac8d9ff9cc761e031308ff7556
Bethany Fitz-Gerald <bethanyfitz-gerald@bethanys-air.lan>
2023-03-01 09:48:28 -0600

    initial project setup

---
+++ src/Pet.jsx
@@ -0,0 +1,11 @@
+const Pet = ({name, animal, breed, images, location, id}) => {
+    let hero = "http://pets-images.dev-apis.com/pets/none.jpg"
+    if(images.length) {
+        hero = images[0];
+    }
+  return (
+    
+  );
+};
+
+export default Pet;
